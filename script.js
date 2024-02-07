69a6d959cf701031b74f35c7752952c2c070c4ec
Giorge Lucas <giorgelukkas@gmail.com>
2024-02-07 14:52:47 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function login() {
+    var senha = document.getElementById("password").value;
+
+    if(senha == "GioAmor") {
+       window.open("./coração.html")
+    }
+    
+    else{
+        alert("errou a senha babe, tenta denovo ;)");
+    
+    }
+    
+    
+}
+
